aa97ecdba90a1d436024afafd83bb07e5142a20e
chien23 <118019128+chien23@users.noreply.github.com>
2022-11-12 03:05:22 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/pvchien.dz';
+const eventName = 'Chúc Mừng 29-11';
+const titleCard = 'HAPPY BIRTHDAY';
+const contentCard = 'Người Anh từng yêu  29/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'https://scontent.fhan3-3.fna.fbcdn.net/v/t1.6435-9/174208560_1286381985088751_2692814409648423558_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rusnshEUs4cAX8tNZcd&_nc_ht=scontent.fhan3-3.fna&oh=00_AfDyUWH_tDeUT9kznJ8V4C1jjGCd8EhJ92JwvsG5KBAyXQ&oe=6395A9D4';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
